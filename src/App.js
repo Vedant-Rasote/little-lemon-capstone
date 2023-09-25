091bf492ee7959e92fed7c808ff2f10300706cb0
Vedant <vedant.d.rasote@gmail.com>
2023-09-25 14:50:58 +0530

    About us sections responsivness fixes

--- src/App.js
+++ src/App.js
@@ -44,16 +44,14 @@ const App = () => (
         <path d="M0 2.26829C315.486 263.797 1161.63 -122.985 1440 44.8544" stroke="black" />
       </svg>
 
-      <div className="row flex-lg-row-reverse align-items-center g-5 pb-5 negative-margin" id='about'>
-        <div className="col-10 col-sm-8 col-lg-6" style={{ backgroundImage: `url(${bgHeroText})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
+      <div className="row align-items-center g-5 py-5 negative-margin" id='about'>
+        <div className="col-10 col-sm-8 col-lg-6 mx-auto" >
+          <img src={about_us_image} className="d-block img-fluid" alt="Bootstrap Themes" loading="lazy" />
+        </div>
+        <div className="col-lg-6 text-center text-lg-start" style={{ backgroundImage: `url(${bgHeroText})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
           <h1 className="fw-bold text-body-emphasis lh-1 mb-3">About Us</h1>
           <p className="lead">We are a family-owned Mediterranean restaurant located in Chicago, Illinois. Our restaurant is known for its focus on traditional recipes served with a modern twist. The chefs draw inspiration from Italian, Greek, and Turkish cultures to create a unique fusion of flavors.</p>
-          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" className="btn btn-warning rounded-pill px-3">More Details ↝</button>
-          </div>
-        </div>
-        <div className="col-lg-6">
-          <img src={about_us_image} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" />
+          <button type="button" className="btn btn-warning rounded-pill px-3">More Details ↝</button>
         </div>
       </div>
 
@@ -175,7 +173,7 @@ const App = () => (
           </svg>
         </div>
         <div className="card w-75 mx-auto border-0 bg-transparent">
-          <div className="card-body brand_green_transparent rounded-2 pb-0">
+          <div className="card-body brand_green_transparent rounded-2 pb-0 text-center text-lg-start">
             <figure>
               <blockquote>
                 <q>
