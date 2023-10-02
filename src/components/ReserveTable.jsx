204eeb9debb62e59c4cea91e080ea60fbc39db0c
Vedant <vedant.d.rasote@gmail.com>
2023-10-02 22:30:58 +0530

    Modified homepage reservation form

--- src/components/ReserveTable.jsx
+++ src/components/ReserveTable.jsx
@@ -1,31 +1,139 @@
-import React from 'react'
-import reserveTableBg from '../images/reserve_table_bg.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import reserveTableBg from "../images/reserve_table_bg.png";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const ReserveTable = () => {
-    return (
-        <section className='mx-auto border-top border-bottom mb-3 glass_morph' id="reserve_table_section" style={{ backgroundImage: `url(${reserveTableBg})`, backgroundSize: 'contain', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }}>
-            <h3 className="my-4 text-center">Reserve a Table</h3>
-            <p className='text-center'>To help us find the best table for you, select the preferred party size, date and time of your reservation.</p>
-            <form className="row g-3">
-                <div className="col-md-4">
-                    <label for="party_size" className="form-label">Party Size</label>
-                    <input type="number" className="form-control" id="party_size" />
-                </div>
-                <div className="col-md-4">
-                    <label for="date" className="form-label">Date</label>
-                    <input type="date" className="form-control" id="time" />
-                </div>
-                <div className="col-md-4">
-                    <label for="time" className="form-label">Time</label>
-                    <input type="time" className="form-control" id="time" />
-                </div>
-                <div className="col-12 text-center">
-                    <Link className="btn btn-dark rounded-pill mb-3" to="/table_reservation_details">Find a Table</Link>
-                </div>
-            </form>
-        </section>
-    )
-}
+  const navigate = useNavigate();
+  const initialValues = {
+    partySize: 0,
+    date: "",
+    time: "",
+  };
 
-export default ReserveTable
+  const onSubmit = (values) => {
+    console.log(values);
+    const { partySize, date, time, } = values;
+    console.log(`/table_reservation_details?partySize=${partySize}`&`date=${date}`&`time=${time}`);
+    navigate(`/table_reservation_details?partySize=${partySize}`&`date=${date}`&`time=${time}`);
+  }
+
+  const validationSchema = Yup.object({
+    partySize: Yup.number()
+      .required("Please enter the number of Guests!")
+      .min(2, "Party size must be at least 2")
+      .max(10, "Party size cannot exceed 10"),
+    date: Yup.date()
+      .required("Date is required")
+      .min(new Date(), "Date must be in the future"),
+    time: Yup.string()
+      .required("Time is required")
+      .oneOf(
+        [
+          "17:00",
+          "17:30",
+          "18:30",
+          "20:00",
+          "20:30",
+          "21:00",
+          "22:00",
+          "23:30",
+        ],
+        "Please select a valid time"
+      ),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit
+  });
+
+  return (
+    <section
+      className="mx-auto border-top border-bottom mb-3 glass_morph"
+      id="reserve_table_section"
+      style={{
+        backgroundImage: `url(${reserveTableBg})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <h3 className="my-4 text-center">Reserve a Table</h3>
+      <p className="text-center">
+        To help us find the best table for you, select the preferred party size,
+        date and time of your reservation.
+      </p>
+      <Form className="row g-3" onSubmit={formik.handleSubmit}>
+        <div className="col-md-4">
+          <Form.Group className="mb-3">
+            <Form.Label>Party Size</Form.Label>
+            <Form.Control
+              type="number"
+              id="partySize"
+              name="partySize"
+              {...formik.getFieldProps("partySize")}
+            />
+            {formik.touched.partySize && formik.errors.partySize ? (
+              <Form.Text className="text-danger">
+                {formik.errors.partySize}
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+        </div>
+        <div className="col-md-4">
+          <Form.Group className="mb-3">
+            <Form.Label>Date</Form.Label>
+            <Form.Control
+              type="date"
+              id="date"
+              name="date"
+              {...formik.getFieldProps("date")}
+            />
+            {formik.touched.date && formik.errors.date ? (
+              <Form.Text className="text-danger">
+                {formik.errors.date}
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+        </div>
+        <div className="col-md-4">
+          <Form.Group className="mb-3">
+            <Form.Label>Time</Form.Label>
+            <Form.Control
+              as="select"
+              id="time"
+              name="time"
+              {...formik.getFieldProps("time")}
+            >
+              <option selected hidden value=""></option>
+              <option value="17:00">17:00</option>
+              <option value="17:30">17:30</option>
+              <option value="18:30">18:30</option>
+              <option value="20:00">20:00</option>
+              <option value="20:30">20:30</option>
+              <option value="21:00">21:00</option>
+              <option value="22:00">22:00</option>
+              <option value="23:30">23:30</option>
+            </Form.Control>
+            {formik.touched.time && formik.errors.time ? (
+              <Form.Text className="text-danger">
+                {formik.errors.time}
+              </Form.Text>
+            ) : null}
+          </Form.Group>
+        </div>
+        <div className="col-12 text-center">
+          <Button type="submit" role="submit" className="btn btn-dark rounded-pill mb-3">
+            Find a Table
+          </Button>
+        </div>
+      </Form>
+    </section>
+  );
+};
+
+export default ReserveTable;
--- src/components/ReserveTableDetails.jsx
+++ src/components/ReserveTableDetails.jsx
@@ -1,34 +1,112 @@
-import React from 'react'
+
+// Date From ReserveTable and display in a step form manner
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 const ReserveTableDetails = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const initialValues = {
+    firstName: '',
+    email: '',
+    type: 'hireMe',
+    comment: '',
+  };
+
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email format').required('Required'),
+    type: Yup.string().required('Required'),
+    comment: Yup.string().required('Required').min(25, 'Must be at least 25 characters'),
+  });
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: async (values) => {
+      // Your form submission logic here
+      // Replace this with your actual API call
+
+      try {
+        // Simulate a server request (remove this in production)
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        setShowAlert(true);
+        formik.resetForm();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
+
   return (
     <>
       <div className="mt-5">
-        <br />
-        <br />
-        {/* The following options are available for a reservation on September 27th, 6:30 pm. */}
-        <p>Work in Progress: Reserve Table feature coming soon!</p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <Form onSubmit={formik.handleSubmit}>
+          <h1>Contact me</h1>
+          <Form.Group className="mb-3">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              id="firstName"
+              name="firstName"
+              {...formik.getFieldProps('firstName')}
+            />
+            {formik.touched.firstName && formik.errors.firstName ? (
+              <Form.Text className="text-danger">{formik.errors.firstName}</Form.Text>
+            ) : null}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              id="email"
+              name="email"
+              {...formik.getFieldProps('email')}
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <Form.Text className="text-danger">{formik.errors.email}</Form.Text>
+            ) : null}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Type of enquiry</Form.Label>
+            <Form.Select id="type" name="type" {...formik.getFieldProps('type')}>
+              <option value="hireMe">Freelance project proposal</option>
+              <option value="openSource">Open source consultancy session</option>
+              <option value="other">Other</option>
+            </Form.Select>
+            {formik.touched.type && formik.errors.type ? (
+              <Form.Text className="text-danger">{formik.errors.type}</Form.Text>
+            ) : null}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Your message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={4}
+              id="comment"
+              name="comment"
+              {...formik.getFieldProps('comment')}
+            />
+            {formik.touched.comment && formik.errors.comment ? (
+              <Form.Text className="text-danger">{formik.errors.comment}</Form.Text>
+            ) : null}
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Submit
+          </Button>
+          {showAlert && (
+            <Alert className="mt-3" variant="success">
+              Form submitted successfully.
+            </Alert>
+          )}
+        </Form>
       </div>
       <hr />
     </>
-  )
-}
+  );
+};
 
-export default ReserveTableDetails
+export default ReserveTableDetails;
+
